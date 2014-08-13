1c6be8b4aac6150a2561c8a5092dacf7256e62f0
Gil Ayalon <gil@monkeytech.co.il>
2014-08-13 13:58:45 +0300

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :default_headers
 
 private
 	def current_user
@@ -11,9 +10,5 @@ private
 	
 	def authorize
 		redirect_to login_url, alert: "Not authorized" if current_user.nil?
-	end	
-
-	def default_headers
-		headers['X-Frame-Options'] = 'ALLOWALL'
-	end	 
+	end 
 end
--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,10 +3,7 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-    puts User.active.all.to_json
     user = User.active.find_by(username: params[:email])
-    puts "!!!!!!!!!!!"
-    puts user.to_json
     return redirect_to :back, alert: 'Invalid email address.' if user.blank? || user.auth_type != User::AUTH_TYPE_PASSWORD
 
     # create a unique password reset token
