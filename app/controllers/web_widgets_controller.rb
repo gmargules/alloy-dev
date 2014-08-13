b1c8b0ba31a1b117100e355abe7bd935d24d0177
Gil Ayalon <gil@monkeytech.co.il>
2014-08-13 15:27:34 +0300

    fix

--- app/controllers/web_widgets_controller.rb
+++ app/controllers/web_widgets_controller.rb
@@ -8,7 +8,12 @@ class WebWidgetsController < ApplicationController
 
 			unless business.blank?
 				product = business.products.find_by(serial: params[:product_id])
+
 				unless product.blank?
+					puts "!!!!!!!!!!!!!!!!!!"
+					puts @current_user.bust
+					puts @current_user.waist
+					puts product.category_id
 					# query the business's size chart
 					@result = business.sizes.where('category_id = :category and ((min_bust <= :bust and max_bust >= :bust) or (min_waist <= :waist and max_waist >= :waist))', category: product.category_id, bust: @current_user.bust, waist: @current_user.waist).first
 				end
