332f86844307f76b9ea2029aca6648e844db58c9
Gil Ayalon <gil@monkeytech.co.il>
2014-08-13 13:14:19 +0300

    fix

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,4 +1,28 @@
 ActiveAdmin.register Product do  
+  form do |f|
+    f.inputs do
+    	f.input :business
+      	f.input :category
+      	f.input :name
+      	f.input :serial
+    end
+    f.actions
+  end
+  
+  filter :name
+  filter :serial
+  
+  index do
+    column :id
+    column :serial
+    column :name
+    column :created_at
+    column :updated_at
+    
+    actions defaults: true do 
+      
+    end 
+  end
 
   controller do
     def permitted_params
