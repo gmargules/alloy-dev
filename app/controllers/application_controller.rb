cb381c1a6909fe842315dc0edbd46aad6dd577cd
Gil Ayalon <gil@monkeytech.co.il>
2014-08-13 14:03:30 +0300

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :default_headers
 
 private
 	def current_user
@@ -10,5 +11,9 @@ private
 	
 	def authorize
 		redirect_to login_url, alert: "Not authorized" if current_user.nil?
-	end 
+	end	
+
+	def default_headers
+		headers['X-Frame-Options'] = 'ALLOWALL'
+	end	 
 end
