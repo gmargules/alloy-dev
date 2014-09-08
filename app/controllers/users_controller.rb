666eb5f42ab6fea15323734348deaf1bc4837c04
Gai Margules <gai@monkeytech.co.il>
2014-09-08 14:56:50 +0300

    size change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,15 @@ class UsersController < ApplicationController
   def edit
   	if @current_user.id != params[:id]
   		return redirect_to :back, alert: 'User does not match'
+  	end
   end
 
   def create
   	if User.find_by(username: params[:username])
   		return redirect_to :back, alert: 'Username already exists'
+  	end
   	if params[:password] != params[:confirm_password]
   		return redirect_to :back, alert: 'Passwords does not match'
   	end
+  end
 end
