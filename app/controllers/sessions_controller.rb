3b350fa9ab89e6dcae85382d20e48af94ed280e4
Gai Margules <gai@monkeytech.co.il>
2014-09-08 10:06:57 +0300

    password_fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
-    if user && user.password
+    if user && user.password == params[:password]
       cookies.permanent[:token] = user.access_token
       redirect_to :back
     else
