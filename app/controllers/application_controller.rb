3a3a44fd093eb2c2ee97b17b7dc943149dbc5650
Gai Margules <gai@monkeytech.co.il>
2014-09-09 10:15:18 +0300

    csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_p3p
 
 private
 	def current_user
@@ -11,4 +12,8 @@ private
 	def authorize
 		redirect_to login_url, alert: "Not authorized" if current_user.nil?
 	end	 
+
+	def set_p3p
+    	headers['P3P'] = 'CP="ALL DSP COR CURa ADMa DEVa OUR IND COM NAV"'
+  	end
 end
